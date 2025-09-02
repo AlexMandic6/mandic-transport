4a62e591986a44fcba2ac29abc01e8a77c789041
Aleksandar Mandic <aleksandar.mandic@jdecoffee.com>
2025-09-02 12:42:10 +0200

    added images, colors, hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>MAIN CONTENT</div>;
+  return (
+    <Image
+      src="/scania.jpg"
+      alt="Scania truck image"
+      width={2048}
+      height={1366}
+      objectFit="cover"
+      priority={true}
+      loading="eager"
+    />
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,14 @@
+import Image from "next/image";
+
 function header() {
-  return <div>header</div>;
+  return (
+    <div
+      className="bg-midnight h-16 text-white flex items-center px-4 gap-2
+"
+    >
+      <Image src="/logo-scania.svg" alt="Scania Logo" width={40} height={40} />
+      <p>header</p>
+    </div>
+  );
 }
 export default header;
